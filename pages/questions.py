d545f8f81064885eda1da87bdc71961808d0419d
Irfan Firosh <irfan.firosh@gmail.com>
2024-03-31 05:05:18 -0400

    Fixing questions

--- pages/questions.py
+++ pages/questions.py
@@ -1,19 +1,27 @@
 import streamlit as st
 import os
 import time
+from GeminiPull import *
+
 
 path = "../metadata/qna/"
 
+def clear_downloads():
+    for file in os.listdir(path):
+        path = os.path.join(path, file)
+        os.remove(path)
+
+
+with st.spinner("Generating Question..."):
+    clear_downloads()
+    while not os.path.exists(f"{path}question.txt"):
+        time.sleep(1)
+    while not os.path.exists(f"{path}answer.txt"):
+        time.sleep(1)
+
 st.title("Questions and Answers")
-
-while not os.path.exists(f"{path}question.txt"):
-    time.sleep(1)
-
 with open(f"{path}question.txt", "r") as file:
     questions = file.read()
-
-while not os.path.exists(f"{path}answer.txt"):
-    time.sleep(1)
 
 with open(f'{path}answer.txt', "r") as file:
     answers = file.read()
